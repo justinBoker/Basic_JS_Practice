5cbb65ec2cde86283824941b3b81ae0985711701
Justin Boker <bokerjustin@gmail.com>
2022-08-19 13:07:43 -0500

    Final Draft

--- Coding_Assignment.js
+++ Coding_Assignment.js
@@ -2,18 +2,13 @@
 let ages = [3, 9, 23, 64, 2, 8, 28, 93];
 
 // Coding Step 1a.
-function subtractFirstToLast () {
-    let first = ages[0];
-    let last = ages[ages.length-1];
-    console.log(last - first);
-}
-subtractFirstToLast();
+console.log(ages[ages.length-1] - ages[0]);
 
-// Coding Step 1b.
+// // Coding Step 1b.
 ages.push(63);
-subtractFirstToLast();
+console.log(ages[ages.length-1] - ages[0]);
 
-// Coding Step 1c.
+// // Coding Step 1c.
 let sum = 0;
 for(let value of ages) {
     sum += value;
@@ -21,10 +16,10 @@ for(let value of ages) {
 let average = sum / ages.length;
 console.log(average);
 
-// Coding Step 2.
+// // Coding Step 2.
 let names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'];
 
-// Coding Step 2a.
+// // Coding Step 2a.
 let sum1 = 0;
 for(let value of names) {
     sum1 += value.length;
@@ -32,42 +27,44 @@ for(let value of names) {
 let average1 = sum1 / names.length;
 console.log(average1);
 
-// Coding Step 2b.
-console.log(names.join(' '));
+// // Coding Step 2b.
+for(i = 5; i < names.length; i++) {
+    console.log(names.join(' '));
+}
 
-// Coding Step 3.
-// Use the array length property
+// // Coding Step 3.
+// // Use the array length property. For example: ages[ages.length-1].
 
-// Coding Step 4.
-// Use the [] operator
+// // Coding Step 4.
+// // Use the [] operator. For example: ages[0].
 
-// Coding Step 5.
+// // Coding Step 5.
 let nameLengths = [];
 for(let value of names) {
     nameLengths.push(value.length);
 }
 console.log(nameLengths);
 
-// Coding Step 6.
+// // Coding Step 6.
 let sum2 = 0;
 for(let value of nameLengths) {
     sum2 += value;
 }
 console.log(sum2);
 
-// Coding Step 7.
+// // Coding Step 7.
 let repeatWord = (word, n) => {
     console.log(word.repeat(n));
 };
 repeatWord('Hello', 3);
 
-// Coding Step 8.
+// // Coding Step 8.
 let createFullName = (firstName, lastName) => {
     console.log(firstName + ' ' + lastName);
 };
 createFullName('Justin', 'Boker');
 
-// Coding Step 9.
+// // Coding Step 9.
 let isGreaterThan100 = array => {
     let sum = 0;
     for(let value of array) {
@@ -75,9 +72,9 @@ let isGreaterThan100 = array => {
     }
     console.log(sum > 100);
 };
-isGreaterThan100([1, 50, 40]);
+isGreaterThan100([50, 50, 40]);
 
-// Coding Step 10.
+// // Coding Step 10.
 let figureAverage = array => {
     let sum = 0;
     for(let value of array) {
@@ -88,7 +85,7 @@ let figureAverage = array => {
 };
 figureAverage([2, 5, 6, 19, 34, 5, 15]);
 
-// Coding Step 11.
+// // Coding Step 11.
 let isFirstArrayGreater = (array1, array2) => {
     let sum1 = 0;
     let sum2 = 0;
@@ -100,41 +97,19 @@ let isFirstArrayGreater = (array1, array2) => {
     }
     let average1 = sum1 / array1.length;
     let average2 = sum2 / array2.length;
-    return average1 > average2; // This will need to be reverted back to console.log(average1 > average 2);
+    return average1 > average2; 
 };
-isFirstArrayGreater([5, 8, 2], [1, 2, 5]);
-let isFirstArrayGreater2 = (array1, array2) => {
-    // let sum1 = 0;
-    // let sum2 = 0;
-    // for(let value1 of array1) {
-    //     sum1 += value1;
-    // }
-    // for(let value2 of array2) {
-    //     sum2 += value2;
-    // }
-    // let average1 = sum1 / array1.length;
-    // let average2 = sum2 / array2.length;
-    if(figureAverage(array1) > figureAverage(array2)) {
-        console.log(figureAverage(array1));
-        console.log(figureAverage(array2));
-        return true;
-    } else {
-        console.log(figureAverage(array1));
-        console.log(figureAverage(array2));
-        return false;
-    }
-};
-console.log(isFirstArrayGreater2([5, 8, 2], [1, 2, 5]));
+console.log("The first array is greater: " + isFirstArrayGreater([5, 8, 2], [20, 2, 5]));
 
-// Coding Step 12.
+// // Coding Step 12.
 let willBuyDrink = (isHotOutside, moneyInPocket) => {
     console.log(isHotOutside && moneyInPocket > 10.50);
 };
-willBuyDrink(false, 10.51);
+willBuyDrink(true, 10.51);
 
-// Coding Step 13.
-// My function takes a number and returns the number and type of coins needed to create that amount.
-// I decided to create this function because I thought that it would be an interesting challenge.
+// // Coding Step 13.
+// // My function takes a number and returns the number and type of coins needed to create that amount.
+// // I decided to create this function because I thought that it would be an interesting challenge.
 
 let quarter = 25;
 let dime = 10;
@@ -176,4 +151,4 @@ let amountToCoins = amount => {
     }
 };
 
-amountToCoins(15041);
+amountToCoins(156);
